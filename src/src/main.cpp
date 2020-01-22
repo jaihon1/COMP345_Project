7183de488d9e340e257f9ce873e49c096605cb09
jaihon1 <jaihon.qasim@hotmail.com>
2020-01-22 17:02:44 -0500

    Created basic mmodule structure for Map.

--- src/src/main.cpp
+++ src/src/main.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
-#include "modules/board/board.hpp"
+#include "modules/board/GBMaps.hpp"
 
 using namespace std;
 
 int main(int argc, const char * argv[]) {
-        
-    Board board1(11);
     
-    cout << "Board ID: " << board1.getId() << endl;
+    int id = 11;
+    int players = 4;
+    
+    GBMaps map1(&id, &players);
+    
+    cout << "Board ID: " << map1.getIdAddress() << endl;
+    cout << "Number of Players: " << map1.getNumberOfPlayers() << endl;
     
     
     return 0;
