000905909a7206707daa0cfc05dff396b6d265d2
JibsonDugnutt <52796563+JibsonDugnutt@users.noreply.github.com>
2020-01-26 17:58:32 -0500

    first draft of header file: adding commonly used public methods

--- src/modules/board/GBMaps.hpp
+++ src/modules/board/GBMaps.hpp
@@ -2,24 +2,43 @@
 #define board_h
 
 class GBMaps {
+
+//MUST CHANGE TO POINTERS LATER
 private:
-    int* _id;
-    int* _numberOfPlayers;
+	int numberOfPlayers;
 
-	int test;
+	//double pointers: one for row and one for column
+	GBSquare** board;
 
-    
-    
 public:
-    GBMaps(int* id, int* numberOfPlayers);
+	//constructor takes number of players and whether the game is played on Side A or Side B
+    GBMaps(int numberOfPlayers, char boardSide);
     ~GBMaps();
     
-    void setId(int* id);
-    void setNumberOfPlayers(int* numberOfPlayers);
-    
-    int getId();
-    int* getIdAddress();
-    int getNumberOfPlayers();
+	GBSquare getSquare(int row, int column);
+
+	GBSquareStatus getSquareStatus(int row, int column);
+
+	//return boolean: true = tile successfully added, false = tile not added (probably because square is unavailable)
+	boolean addHarvestTile(int row, int column);
+
+	//return boolean: true = tile successfully added, false = tile not added (probably because square is unavailable)
+	boolean addPondTile(int row, int column);
+
+};
+
+//Game Board is an array of GBSquare objects identified by row and column number
+class GBSquare {
+	GBSquareStatus status;
+
+};
+
+//enumerated class to give name to the status that a square could have
+enum class GBSquareStatus {
+	Empty, //associated square holds nothing (empty)
+	HarvestTile, //associated square holds a harvest tile
+	PondTile, //associated square holds a pond tile (Side B)
+	Unavailable //when playing with four players the corner squares are unavailable
 };
 
 #endif /* board_h */
