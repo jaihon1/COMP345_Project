67369702c1de2127dfc6a41ca162b90351431581
JibsonDugnutt <52796563+JibsonDugnutt@users.noreply.github.com>
2020-01-26 22:48:35 -0500

    Begin implementation: implemented constructor

--- src/modules/board/GBMaps.cpp
+++ src/modules/board/GBMaps.cpp
@@ -1,36 +1,64 @@
 #include <stdio.h>
 #include "GBMaps.hpp"
 
-// Constructor
-GBMaps::GBMaps(int* id, int* numberOfPlayers) {
-    _id = id;
-    _numberOfPlayers = numberOfPlayers;
+GBMaps::GBMaps(int numberOfPlayers, char boardSide)
+{ 
+	//TODO: enter if statement on each case for Board B option
+	switch (numberOfPlayers) {
+	case 2: 
+		rows = new int(5);
+		columns = new int(5);
+		initializeBoard();
+
+		board[0][0].status = GBSquareStatus::HarvestTile;
+		board[0][4].status = GBSquareStatus::HarvestTile;
+		board[4][0].status = GBSquareStatus::HarvestTile;
+		board[4][4].status = GBSquareStatus::HarvestTile;
+		break;
+	case 3:
+		//hard code 3 pleayer game along horizontal axis (as opposed to letting players choose orientation)
+		rows = new int(5);
+		columns = new int(7);
+		initializeBoard();
+
+		board[0][1].status = GBSquareStatus::HarvestTile;
+		board[0][5].status = GBSquareStatus::HarvestTile;
+		board[4][1].status = GBSquareStatus::HarvestTile;
+		board[4][5].status = GBSquareStatus::HarvestTile;
+		break;
+	case 4:
+		rows = new int(7);
+		columns = new int(7);
+		initializeBoard();
+
+		board[0][0].status = GBSquareStatus::Unavailable;
+		board[0][6].status = GBSquareStatus::Unavailable;
+		board[6][0].status = GBSquareStatus::Unavailable;
+		board[6][6].status = GBSquareStatus::Unavailable;
+
+		board[1][1].status = GBSquareStatus::HarvestTile;
+		board[1][5].status = GBSquareStatus::HarvestTile;
+		board[5][1].status = GBSquareStatus::HarvestTile;
+		board[5][5].status = GBSquareStatus::HarvestTile;
+		break;
+	default:
+		//if an invalid number of players are given then a "null" board is created
+		board = NULL;
+		break;
+	}
+
 }
 
-// Destructor
-GBMaps::~GBMaps(){
-    printf("Object board with ID = %p has been destructed!\n", _id);
-};
+void GBMaps::initializeBoard() {
+	
+	board = new GBSquareArrayPtr[*rows];
+	
+	for (int i = 0; i < *rows; i++) {
+		board[i] = new GBSquare[*columns];
 
-// Methods
-void GBMaps::setId(int* id) {
-    _id = id;
+		//inner loop to initialize GBSquares
+		for (int j = 0; j < *columns; j++) {
+			board[i][j].status = GBSquareStatus::Empty;
+		}
+	}
 }
-
-void GBMaps::setNumberOfPlayers(int* numberOfPlayers) {
-    _numberOfPlayers = numberOfPlayers;
-}
-
-int GBMaps::getId() {
-    return *_id;
-}
-
-int* GBMaps::getIdAddress() {
-    return _id;
-}
-
-int GBMaps::getNumberOfPlayers() {
-    return *_numberOfPlayers;
-}
-
-
--- src/modules/board/GBMaps.hpp
+++ src/modules/board/GBMaps.hpp
@@ -1,44 +1,51 @@
 #ifndef board_h
 #define board_h
 
-class GBMaps {
-
-//MUST CHANGE TO POINTERS LATER
-private:
-	int numberOfPlayers;
-
-	//double pointers: one for row and one for column
-	GBSquare** board;
-
-public:
-	//constructor takes number of players and whether the game is played on Side A or Side B
-    GBMaps(int numberOfPlayers, char boardSide);
-    ~GBMaps();
-    
-	GBSquare getSquare(int row, int column);
-
-	GBSquareStatus getSquareStatus(int row, int column);
-
-	//return boolean: true = tile successfully added, false = tile not added (probably because square is unavailable)
-	boolean addHarvestTile(int row, int column);
-
-	//return boolean: true = tile successfully added, false = tile not added (probably because square is unavailable)
-	boolean addPondTile(int row, int column);
-
-};
-
-//Game Board is an array of GBSquare objects identified by row and column number
-class GBSquare {
-	GBSquareStatus status;
-
-};
-
 //enumerated class to give name to the status that a square could have
 enum class GBSquareStatus {
 	Empty, //associated square holds nothing (empty)
 	HarvestTile, //associated square holds a harvest tile
 	PondTile, //associated square holds a pond tile (Side B)
 	Unavailable //when playing with four players the corner squares are unavailable
+};
+
+//Game Board is an array of GBSquare objects identified by row and column number
+class GBSquare {
+public:
+	GBSquareStatus status;
+};
+
+class GBMaps {
+
+private:
+	//no length function for arrays in c++
+	int* rows;
+	int* columns;
+
+	//an array of pointers that point to GBSquare objects
+	typedef GBSquare* GBSquareArrayPtr;
+
+	//Game Board is a pointer to an array of pointers
+	GBSquareArrayPtr *board;
+
+	void initializeBoard();
+
+public:
+	//constructor takes number of players and whether the game is played on Side A or Side B
+    GBMaps(int numberOfPlayers, char boardSide);
+	
+    ~GBMaps();
+    
+	GBSquare getSquare(int row, int column);
+
+	GBSquareStatus getSquareStatus(int row, int column);
+
+	//return int: 1 = tile successfully added, 0 = tile not added (probably because square is unavailable)
+	int addHarvestTile(int row, int column);
+
+	//return boolean: true = tile successfully added, false = tile not added (probably because square is unavailable)
+	int addPondTile(int row, int column);
+
 };
 
 #endif /* board_h */
