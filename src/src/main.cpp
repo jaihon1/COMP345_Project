ee6a9a7446de97ad73105a987366a038550649bc
jaihon1 <jaihon.qasim@hotmail.com>
2020-01-31 15:53:02 -0500

    Started creation of Player and Loader.

--- src/src/main.cpp
+++ src/src/main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include "modules/board/GBMaps.hpp"
+#include "modules/loader/loader.hpp"
+#include "modules/player/player.hpp"
 
 using namespace std;
+
+#include <fstream>
 
 int main(int argc, const char * argv[]) {
     
@@ -9,10 +13,24 @@ int main(int argc, const char * argv[]) {
     int players = 4;
     
     GBMaps map1(&id, &players);
+    Loader myLoader;
     
-    cout << "Board ID: " << map1.getIdAddress() << endl;
-    cout << "Number of Players: " << map1.getNumberOfPlayers() << endl;
     
+//    ofstream myfile ("/Users/dzhay/Github/COMP345_Project/src/jirt.txt");
+//    if (myfile.is_open())
+//    {
+//      myfile << "This is a line.\n";
+//      myfile << "This is another line.\n";
+//      myfile.close();
+//    }
+//    else cout << "Unable to open file";
+
+    
+    myLoader.read("/Users/dzhay/Github/COMP345_Project/src/test.txt");
+//    
+//    cout << "Board ID: " << map1.getIdAddress() << endl;
+//    cout << "Number of Players: " << map1.getNumberOfPlayers() << endl;
+        
     
     return 0;
 }
