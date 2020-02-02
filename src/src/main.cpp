54012202253c1ac15db303641b7df81a3b43720e
JibsonDugnutt <52796563+JibsonDugnutt@users.noreply.github.com>
2020-02-01 23:04:05 -0500

    restored main.cpp from master

---
+++ src/src/main.cpp
@@ -0,0 +1,19 @@
+  
+#include <iostream>
+#include "modules/board/GBMaps.hpp"
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    
+    int id = 11;
+    int players = 4;
+    
+    GBMaps map1(&id, &players);
+    
+    cout << "Board ID: " << map1.getIdAddress() << endl;
+    cout << "Number of Players: " << map1.getNumberOfPlayers() << endl;
+    
+	system("pause");
+    return 0;
+}
