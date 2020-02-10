cdadf3598a0e34bb24bcafe42af821405b5e29d2
tiffhc <46658494+tiffhc@users.noreply.github.com>
2020-02-10 11:48:49 -0500

    Updated VGMaps (2)

--- src/src/modules/board/VGMaps.hpp
+++ src/src/modules/board/VGMaps.hpp
@@ -3,18 +3,18 @@
 #include "..\tile\BuildingTile.hpp"
 
 
-class VGSquare {
-public:
-	
+//not sure if I need 
 
-	VGSquare(); 
-	~VGSquare(); 
-
-
-};
 class VGMaps {
 private:
 	
+	const int* const row s = new int(6);
+	const int* const colums = new int(7); 
+
+	bool GreenSheepPlaced;
+	bool GreyRockPlaced; 
+	bool RedLumber; 
+	bool YellowHay; 
 	//pointer to the map
 	VGMaps * Map; 
 	
@@ -23,7 +23,7 @@ public:
 
 	~VGMaps(); 
 
-	VGSquare ** village_board; 
+	BuildingTile ** village_board; 
 
 };
 
