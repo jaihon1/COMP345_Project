2718130371308723381da44b70c58e81b5592c74
jaihon1 <jaihon.qasim@hotmail.com>
2020-02-12 13:16:59 -0500

    Started implementing HarvestDeck module.

---
+++ src/src/modules/deck/HarvestDeck.cpp
@@ -0,0 +1,13 @@
+#include "HarvestDeck.h"
+
+HarvestDeck::HarvestDeck() {
+    
+}
+
+HarvestDeck::HarvestDeck(const HarvestDeck &deck) {
+    
+}
+
+HarvestDeck::~HarvestDeck() {
+    
+}
---
+++ src/src/modules/deck/HarvestDeck.h
@@ -0,0 +1,19 @@
+#ifndef HarvestDeck_hpp
+#define HarvestDeck_hpp
+
+#include <stdio.h>
+#include "HarvestTile.h"
+
+using namespace std;
+
+class HarvestDeck {
+private:
+        
+public:
+    HarvestDeck();
+    HarvestDeck(const HarvestDeck &deck);
+    ~HarvestDeck();
+        
+};
+
+#endif /* HarvestDeck_h */
