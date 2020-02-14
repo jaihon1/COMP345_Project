25213203d016b24a70071194e5e88a1830f94526
nguyen <nguyennguyen8251@gmail.com>
2020-02-14 01:28:33 -0500

    setup

--- src/src/testdriver/TestDriver.cpp
+++ src/src/testdriver/TestDriver.cpp
@@ -79,7 +79,7 @@ void testFunction1() {
 }
 
 void testFunction2() {
-	GBMapLoader* testLoader = new GBMapLoader("C:\\Users\\Damian\\Documents\\Repos\\COMP345_Project\\data\\testMap.json");
+	GBMapLoader* testLoader = new GBMapLoader("C:\\Users\\Admin\\Documents\\COMP345_Project\\data\\testMap.json");
 	printGameBoard(testLoader->getBoard());
 }
 
