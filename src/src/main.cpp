39be9c6def4544c3421270ea59636e769f41affc
jaihon1 <jaihon.qasim@hotmail.com>
2020-02-25 22:08:49 -0500

    Fixed merge conflicts with master.

--- src/src/main.cpp
+++ src/src/main.cpp
@@ -96,3 +96,90 @@ int main(int argc, const char * argv[]) {
 
     return 0;
 }
+
+
+
+// MAIN FUNCTIONS FROM A main.cpp FILE OUTSIDE src DRIECTORY !!!
+/*
+int main()
+{
+	BuildingColorType * type1 = new BuildingColorType(BuildingColorType::GreenSheep); 
+	BuildingStatus *status1 = new BuildingStatus(BuildingStatus::Normal); 
+
+	BuildingTile t1(type1, status1); //on stack? do I delete?
+	cout << t1.getBuildingNum() << endl;
+
+	BuildingColorType * type2 = new BuildingColorType(BuildingColorType::RedLumber);
+	BuildingStatus *status2 = new BuildingStatus(BuildingStatus::Normal);
+	BuildingTile t2(type2, status2); //why??? 
+
+	cout << t2.getBuildingNum() << endl;
+
+	cout << "Created building tile"; 
+
+	//A a(); stack
+	//A* aPtr = &a; 
+
+	//driver?
+	return 0; 
+}
+*/
+
+//main for VGMaps
+
+/**/
+// int main()
+// {
+// 	//test 
+// 	//VGMaps *map = new VGMaps();
+// 	VGMaps* v =  &VGMaps();
+// 
+// 	VGMaps* gg(); //most vexing part annoying error 
+// 	//int r = *(v.rows);
+// 	//int c = *(v.columns); 
+// 
+// 	//int *r = map->rows; 
+// 	//int *c = map->columns; 
+// 
+// 	int ro = *(v->rows); 
+// 	int co = *(v->columns); 
+// 
+// 	for (int i = 0; i < ro; i++)
+// 	{
+// 		for (int j = 0;  j < co; j++)
+// 		{
+// 			cout << "TILE ";
+// 		}
+// 		cout << "\n";
+// 		cout << "==========================" << endl; 
+// 	}
+// 	cout << "created VGMap"; 
+// 
+// 	
+// 	//enum function with a switch statements 
+// 	//give an example of VGMaps filled 
+// 	//edits 
+// 
+// 	BuildingColorType * type1 = new BuildingColorType(BuildingColorType::GreenSheep);
+// 	BuildingStatus *status1 = new BuildingStatus(BuildingStatus::Normal);
+// 
+// 	BuildingTile t1(type1, status1); //on stack? do I delete?
+// 
+// 	v->addNewBuildingTile(t1, 1, 1); 
+// 	cout << "added new tile" << endl; 
+// 
+// 	for (int i = 0; i < ro; i++)
+// 	{
+// 		for (int j = 0; j < co; j++)
+// 		{
+// 			//BuildingColorType e =
+// 			
+// 		}
+// 		cout << "\n";
+// 		cout << "==========================" << endl;
+// 	}
+// 	cout << "created VGMap";
+// 
+// 
+// 	return 0; 
+// }
--- src/src/modules/tile/Resources.cpp
+++ src/src/modules/tile/Resources.cpp
@@ -5,6 +5,8 @@
 
 using namespace std;
 
+static bool _firstBuilding = true;
+
 
 HarvestTile::HarvestTile(ResourceName topLeftRes, ResourceName topRightRes, ResourceName bottomLeftRes, ResourceName bottomRightRes)
 {
@@ -51,49 +53,89 @@ ResourceName HarvestTile::getResource(ResourceLocation inLocation)
 	return resourceArr[inLocation];
 }
 
-BuildingTile::BuildingTile(BuildingColorType type, BuildingStatus status) {
-	_buildingColorType = type;
-	_buildingStatus = status;
-	_buildingNum = generateBuildingNumber();
+BuildingTile::BuildingTile()
+{
+    //
+    _firstBuilding = false;
 }
 
-BuildingTile::BuildingTile(BuildingColorType t, int n, BuildingStatus s)
+BuildingTile::BuildingTile(BuildingColorType *type, BuildingStatus *status) : _buildingColorType(type), _buildingStatus(status), _buildingNum(generateBuildingNumber())
 {
-	_buildingColorType = t;
-	_buildingNum = n;
-	_buildingStatus = s;
+    //*_buildingColorType = type;
+    //*_buildingStatus = status;
+    //_buildingNum = generateBuildingNumber();
+
+    _firstBuilding = false;
+}
+
+BuildingTile::BuildingTile(BuildingColorType *t, int *n, BuildingStatus *s): _buildingColorType(t), _buildingNum(n), _buildingStatus(s)
+{
+    /*
+    _buildingColorType = t;
+    _buildingNum = n;
+    _buildingStatus = s;
+    */
+    _firstBuilding = false;
 }
 
 BuildingTile::~BuildingTile() {
-	//do I need to code? 
+    //do I need to code?
+    //if the member variables dont go on the stack, make them dynamic objects and delete every
+    //random to delete
+    //delete _int
+
+//    cout << "in destr" << endl;
+//    delete _buildingColorType;
+//    _buildingColorType = NULL;
+//    delete _buildingStatus;
+//    _buildingStatus = NULL;
+
+    //delete _buildingNum;
+    //_buildingNum = NULL;
+
 }
 
 BuildingColorType BuildingTile::getBuildingColorType() {
-	return _buildingColorType;
+    return *_buildingColorType;
 }
 
 BuildingStatus BuildingTile::getSide() {
-	return _buildingStatus;
+    return *_buildingStatus;
 }
 
 int BuildingTile::getBuildingNum() {
-	return _buildingNum;
+    return *_buildingNum;
 }
 
-int BuildingTile::generateBuildingNumber() {
-	int random = rand() % 6 + 1; //generate a random number from 1 to 6
-	return random;
+
+int *BuildingTile::generateBuildingNumber() {
+    if (_firstBuilding)
+    {
+        srand(time(NULL));  //set the seed first
+    }
+    cout << "Randomizing" << endl;
+    int* random = new int(rand() % 6 + 1); //generate a random number from 1 to 6                                                                              //srand and seed it...
+    //int rando = rand() % 6 + 1; //generate a random number from 1 to 6
+    //int* random = &rando; //generate a random number from 1 to 6
+    return random;
 }
 
 void BuildingTile::setBuildingNum(int num) {
-	_buildingNum = num;
+    *_buildingNum = num;
+}
+
+void BuildingTile::setBuildingStatus(BuildingStatus s)
+{
+    *_buildingStatus = s;
 }
 
 void BuildingTile::flip() {
-	if (getSide() == BuildingStatus::Normal) {
-		setBuildingNum(-1);
-	}
-	//cannot flip back
+    if (getSide() == BuildingStatus::Normal) {
+        setBuildingNum(-1);
+        setBuildingStatus(BuildingStatus::Flipped);
+    }
+    //cannot flip back
+
 }
 
 HarvestDeck::HarvestDeck()
@@ -220,15 +262,15 @@ BuildingDeck::BuildingDeck() {
     BuildingStatus normal = BuildingStatus::Normal;
     
     BuildingTile tiles[9] = {
-        BuildingTile(greenSheep, normal),
-        BuildingTile(greyRock, normal),
-        BuildingTile(redLumber, normal),
-        BuildingTile(yellowHay, normal),
-        BuildingTile(greenSheep, normal),
-        BuildingTile(greenSheep, normal),
-        BuildingTile(greyRock, normal),
-        BuildingTile(redLumber, normal),
-        BuildingTile(yellowHay, normal)
+        BuildingTile(&greenSheep, &normal),
+        BuildingTile(&greyRock, &normal),
+        BuildingTile(&redLumber, &normal),
+        BuildingTile(&yellowHay, &normal),
+        BuildingTile(&greenSheep, &normal),
+        BuildingTile(&greenSheep, &normal),
+        BuildingTile(&greyRock, &normal),
+        BuildingTile(&redLumber, &normal),
+        BuildingTile(&yellowHay, &normal)
     };
     
     // Initializing _deck vector
