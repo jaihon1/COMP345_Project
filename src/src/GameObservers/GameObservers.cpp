e027c49d7237a70599dd26b0ef3d5905581d7836
tiffhc <46658494+tiffhc@users.noreply.github.com>
2020-04-18 21:54:46 -0400

    Updated game Observer code

--- src/src/GameObservers/GameObservers.cpp
+++ src/src/GameObservers/GameObservers.cpp
@@ -39,34 +39,39 @@ void GameObservers::turndisplay()
 			//Check for if player is building harvest tile or building tile?
 
 			//Printing player id
-			turnConsole.cout << "Display current turn" << endl;
-			turnConsole.cout << "Current player ID: " << _subject->getCurrentPlayer(i)->getID() << endl; 
+			turnConsole.cout("Display current turn \n"); 
+			turnConsole.cout("Current player ID: ", _subject->getCurrentPlayer(i)->getID(), "\n"); 
 
 			//Display action for harvest/shipment tile or building Tile? 
 			if (_subject->getPlayerAction() == 1)
 			{
-				turnConsole.cout << "Placed a harvest tile" << endl; 
+				turnConsole.cout("Placed a harvest tile \n"); 
 			}
 			else if (_subject->getPlayerAction() == 2)
 			{
-				turnConsole.cout << "Placed a shipment tile" << endl; 
+				turnConsole.cout("Placed a shipment tile \n"); 
 			}
 			else if (_subject->getPlayerAction() == 3)
 			{
-				turnConsole.cout << "Adding a building to village board" << endl;
+				turnConsole.cout("Adding a building to village board \n"); 
 			}
 			else if (_subject->getPlayerAction() == 4)
 			{
-				turnConsole.cout << "Player is not building anything" << endl; 
+				turnConsole.cout("Player is not building anything \n"); 
 			}
 			else
 			{
-				turnConsole.cout << "Player not doing the right thing" << endl;  
+				turnConsole.cout("Player not doing the right thing \n");  
 			}
 
 			//show ressources - but where is this going to print? to test 
-			_subject->getScoringObject()->display_res; 
-
+			turnConsole.cout("Displaying current ressource tracker: \n"); 
+			turnConsole.cout("Lumber: ", _subject->getScoringObject()->get_lumber()); 
+			turnConsole.cout("Sheep: ", _subject->getScoringObject()->get_sheep());
+			turnConsole.cout("Wheat: ", _subject->getScoringObject()->get_wheat());
+			turnConsole.cout("Rock: ", _subject->getScoringObject()->get_stone());
+			
+			
 			//print number of building tiles - add code when merge with Nguyen's new scoring 
 
 
